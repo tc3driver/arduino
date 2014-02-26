68df4fb57dc170b26e5de4ac9b1acb313928a924
tc3driver <tc3driver@gmail.com>
2014-02-26 01:15:51 -0800

    big oopsie

--- examples/cc3000/buildtest/buildtest.ino
+++ examples/cc3000/buildtest/buildtest.ino
@@ -44,7 +44,7 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
                                          SPI_CLOCK_DIVIDER); // you can change this clock speed but DI
 
 #define WLAN_SSID       "user"        // cannot be longer than 32 characters!
-#define WLAN_PASS       "pass"
+#define WLAN_PASS       "password"
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
